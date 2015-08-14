5c0b1d356472c3aeba64676ceea416272812c4a5
Jérémy Lesceau <jeremy.lesceau@gmail.com>
2015-08-14 20:11:36 +0200

    Formatted header title

--- src/views/header.jsx
+++ src/views/header.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react'),
+    utils = require('../utils.js'),
     BaobabBranchMixin = require('baobab-react/mixins').branch;
 
 module.exports = React.createClass({
@@ -27,7 +28,7 @@ module.exports = React.createClass({
         </div>
         <span className="title">
           <span className="titleView">{
-            this.state.currentView
+            utils.formatTitle(this.state.currentView)
           }</span>
         </span>
       </div>
